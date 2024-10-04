a434cb46efa596ecda03e5e95fb573ff8a593aa9
anupkumarmridha <anupkumarmridha.net@gmail.com>
2024-10-04 12:49:50 +0530

    fixed div center

--- tripit-clone/src/components/Home/BlogSection.tsx
+++ tripit-clone/src/components/Home/BlogSection.tsx
@@ -9,13 +9,13 @@ const BlogSection: React.FC = () => {
             <div className="blog-list flex justify-center px-4 py-4">
                 {blogPosts.map((blog, index) => {
                     return (
-                        <div className="blog-item m-4 w-full" key={index}>
-                            <div className="flex w-[358px] h-[238px] box-border justify-center text-center items-center">
-                                <img src={blog.image} alt="blog" className="w-full h-full object-cover" />
+                        <div className="blog-item m-4 justify-self-center box-border rounded-md flex flex-col items-center" key={index}>
+                            <div className="flex w-[358px] h-[238px] box-border rounded-md justify-center text-center items-center">
+                                <img src={blog.image} alt="blog" className="w-full h-full object-cover box-border rounded-md" />
                             </div>
                             <div className="flex flex-col text-left mt-4">
-                                <a href="" className="mb-4 text-gray-950 text-md font-semibold">{blog.title}</a>
-                                <p>{blog.description}</p>
+                                <a href={blog.link} className="mb-4 text-gray-950 text-md hover:text-[#107ac5] font-semibold">{blog.title}</a>
+                                <p className="box-border">{blog.description}</p>
                             </div>
                         </div>
                     )
