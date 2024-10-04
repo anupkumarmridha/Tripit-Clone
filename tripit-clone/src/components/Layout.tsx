039f9820944d3e0c7defb54ed3a365b2f9c398bf
anupkumarmridha <anupkumarmridha.net@gmail.com>
2024-10-04 15:00:04 +0530

    fixed resposniveness

--- tripit-clone/src/components/Layout.tsx
+++ tripit-clone/src/components/Layout.tsx
@@ -5,9 +5,11 @@ import { ReactNode } from 'react';
 
 const Layout: React.FC<{ children: ReactNode }> = ({ children }) => {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen overflow-hidden">
       <Header />
-      <main className="flex-grow">{children}</main>
+      <main className="flex-grow p-0 md:p-4">
+        {children}
+      </main>
       <Footer />
     </div>
   );
